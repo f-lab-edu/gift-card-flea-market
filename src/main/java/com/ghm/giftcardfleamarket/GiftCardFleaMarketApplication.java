8e10b180950227fcddbb0def6368459f823d1470
ghm8614 <ghm8614@gmail.com>
2023-04-19 19:36:30 +0900

    [#1] feat: 프로젝트 생성

---
+++ src/main/java/com/ghm/giftcardfleamarket/GiftCardFleaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ghm.giftcardfleamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCardFleaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftCardFleaMarketApplication.class, args);
+	}
+
+}
