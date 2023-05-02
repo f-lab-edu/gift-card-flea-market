2f3f9367d6555242d511bc3680929fbd18a1f78c
ghm8614 <ghm8614@gmail.com>
2023-05-03 05:06:10 +0900

    [#8] refactor: 리뷰 반영
    리뷰 반영 및 회원가입한 시간인 createAt 값을 코드 레벨에서 생성하는 것으로 변경

--- src/main/java/com/ghm/giftcardfleamarket/user/dto/request/SignUpRequest.java
+++ src/main/java/com/ghm/giftcardfleamarket/user/dto/request/SignUpRequest.java
@@ -1,5 +1,7 @@
 package com.ghm.giftcardfleamarket.user.dto.request;
 
+import java.time.LocalDateTime;
+
 import com.ghm.giftcardfleamarket.user.domain.User;
 
 import jakarta.validation.constraints.Email;
@@ -9,21 +11,15 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
-/**
- * NotBlank: null, 빈 문자열(""), 공백 문자열(" ") 모두 허용 X
- * Email: 빈 문자열과 공백을 허용하지 않지만, null을 허용하기 때문에 @NotNull 추가
- * Pattern: 010-xxxx-xxxx 형식에 부합하는지 확인
- */
-
 @Getter
 public class SignUpRequest {
 
 	@NotBlank(message = "아이디 입력은 필수입니다.")
-	@Size(min = 6, max = 12, message = "아이디는 6자리 이상 12자 이하여야 합니다.")
+	@Size(min = 6, max = 12, message = "아이디는 {min}자리 이상 {max}자 이하여야 합니다.")
 	private String userId;
 
 	@NotBlank(message = "비밀번호 입력은 필수입니다.")
-	@Size(min = 8, max = 16, message = "비밀번호는 8자 이상 16자 이하여야 합니다.")
+	@Size(min = 8, max = 16, message = "비밀번호는 {min}자 이상 {max}자 이하여야 합니다.")
 	private String password;
 
 	@NotNull(message = "이메일 입력은 필수입니다.")
@@ -34,13 +30,13 @@ public class SignUpRequest {
 	@Pattern(regexp = "^(010)-\\d{4}-\\d{4}$", message = "휴대폰 번호 형식에 맞게 입력해 주세요.")
 	private String phone;
 
-	// SignUpRequest(UserDto)를 Entity(User)에 바인딩하는 메서드
 	public User toEntity(String encryptedPassword) {
 		return User.builder()
 			.userId(userId)
 			.password(encryptedPassword)
 			.email(email)
 			.phone(phone)
+			.createdAt(LocalDateTime.now())
 			.build();
 	}
 }
