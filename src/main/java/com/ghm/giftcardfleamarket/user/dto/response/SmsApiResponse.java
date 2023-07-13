ab56cb153b7a8a8b4c0f2bebc158f157b479e5e1
ghm8614 <ghm8614@gmail.com>
2023-07-14 00:55:42 +0900

    [#11] refactor: 빌더 패턴 대신 일반 생성자로 객체 생성
    1. 기본 생성자가 없어 Json 요청 데이터가 객체로 바인딩 되지 않는 문제 해결
    2. 두개 이하의 필드를 가진 객체는 빌더 패턴 대신 일반 생성자를 사용해서 객체 생성

--- src/main/java/com/ghm/giftcardfleamarket/user/dto/response/SmsApiResponse.java
+++ src/main/java/com/ghm/giftcardfleamarket/user/dto/response/SmsApiResponse.java
@@ -1,11 +1,18 @@
 package com.ghm.giftcardfleamarket.user.dto.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class SmsApiResponse {
 	private String requestId;
 	private String requestTime;
 	private String statusCode;
 	private String statusName;
+
+	public SmsApiResponse(String statusCode, String statusName) {
+		this.statusCode = statusCode;
+		this.statusName = statusName;
+	}
 }
