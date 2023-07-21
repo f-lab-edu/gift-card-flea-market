7d004708dad01086a504847338973f64e582063d
ghm8614 <ghm8614@gmail.com>
2023-07-21 20:35:44 +0900

    [#23] feat: 로그인 실패 예외 추가

---
+++ src/main/java/com/ghm/giftcardfleamarket/user/exception/PasswordMisMatchException.java
@@ -0,0 +1,8 @@
+package com.ghm.giftcardfleamarket.user.exception;
+
+public class PasswordMisMatchException extends RuntimeException {
+
+	public PasswordMisMatchException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/ghm/giftcardfleamarket/user/exception/UnauthorizedUserException.java
@@ -0,0 +1,8 @@
+package com.ghm.giftcardfleamarket.user.exception;
+
+public class UnauthorizedUserException extends RuntimeException {
+
+	public UnauthorizedUserException(String message) {
+		super(message);
+	}
+}
