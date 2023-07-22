0cf3a425272dee617ce3fbfa74fbee4e64974703
ghm8614 <ghm8614@gmail.com>
2023-07-22 16:50:57 +0900

    [#23] refactor: 빌더 패턴 대신 일반 생성자로 객체 생성

--- src/main/java/com/ghm/giftcardfleamarket/user/dto/request/LoginRequest.java
+++ src/main/java/com/ghm/giftcardfleamarket/user/dto/request/LoginRequest.java
@@ -2,9 +2,13 @@ package com.ghm.giftcardfleamarket.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
 	@NotBlank(message = "아이디 입력은 필수입니다.")
 	@Size(min = 6, max = 12, message = "아이디는 {min}자리 이상 {max}자 이하여야 합니다.")
