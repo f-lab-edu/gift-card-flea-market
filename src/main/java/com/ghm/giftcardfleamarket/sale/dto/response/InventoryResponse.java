39600f09575c3d4c3c785ed887ea94878577d597
ghm8614 <ghm8614@gmail.com>
2023-08-28 14:36:32 +0900

    [#33] feat: InventoryResponse 필드 추가

--- src/main/java/com/ghm/giftcardfleamarket/sale/dto/response/InventoryResponse.java
+++ src/main/java/com/ghm/giftcardfleamarket/sale/dto/response/InventoryResponse.java
@@ -2,7 +2,7 @@ package com.ghm.giftcardfleamarket.sale.dto.response;
 
 import java.time.LocalDate;
 
-import com.ghm.giftcardfleamarket.sale.domain.Inventory;
+import com.ghm.giftcardfleamarket.sale.dto.Inventory;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -10,12 +10,16 @@ import lombok.Getter;
 @Getter
 @Builder
 public class InventoryResponse {
+	private String brandName;
+	private String itemName;
 	private LocalDate expirationDate;
 	private int count;
 	private int salePrice;
 
-	public static InventoryResponse of(Inventory inventory, int salePrice) {
+	public static InventoryResponse of(Inventory inventory, String brandName, String itemName, int salePrice) {
 		return InventoryResponse.builder()
+			.brandName(brandName)
+			.itemName(itemName)
 			.expirationDate(inventory.getExpirationDate())
 			.count(inventory.getCount())
 			.salePrice(salePrice)
