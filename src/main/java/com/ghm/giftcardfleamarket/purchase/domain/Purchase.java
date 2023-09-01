fe396a4dd5a67d9834e19caf6aa6b38fbc185c17
ghm8614 <ghm8614@gmail.com>
2023-09-01 15:37:57 +0900

    [#36] feat: Purchase 도메인 객체 추가

---
+++ src/main/java/com/ghm/giftcardfleamarket/purchase/domain/Purchase.java
@@ -0,0 +1,21 @@
+package com.ghm.giftcardfleamarket.purchase.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Purchase {
+	private Long id;
+	private Long saleId;
+	private Long itemId;
+	private String buyerId;
+	private int price;
+	private boolean expirationStatus;
+	private boolean useStatus;
+	private LocalDateTime boughtAt;
+	private LocalDateTime usedAt;
+	private LocalDateTime updatedAt;
+}
